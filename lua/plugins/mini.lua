1f55fa416b47defa217ce1352899f18140d8a557
sleepylog <sleepylog@192.168.68.51>
2025-09-07 02:07:12 +0800

    added in mini.lua suround and pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,4 +1,12 @@
 return {
   "nvim-mini/mini.nvim",
-  version = false
+  version = false,
+  config = function()
+    require("mini.surround").setup()
+    require("mini.pairs").setup()
+  end,
 }
+--[[
+V sa<?> to add <?> around visual selection
+V sd<?> to delete <?> when within the <?>
+]]--

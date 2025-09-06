73a09b5e691ca6848a3a57e5eea79121e4023f65
sleepylog <sleepylog@192.168.68.51>
2025-09-07 02:44:26 +0800

    set indentspace 2

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,11 +15,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
+local indentspace = 2
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.o.tabstop = indentspace
+vim.o.shiftwidth = indentspace
+vim.o.expandtab = true
 
 -- Setup lazy.nvim
 require("lazy").setup({

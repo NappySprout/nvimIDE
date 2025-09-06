9ce18e2a91c0dfff208eb04e738158b04d5553f7
sleepylog <sleepylog@192.168.68.51>
2025-09-07 02:40:11 +0800

    added autocomplete with enter

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  -- use a release tag to download pre-built binaries
+  version = '1.*',
+  opts = {
+    keymap = {
+      preset = 'default',
+      ['<CR>'] = { 'select_and_accept' },
+    },
+  },
+  opts_extend = { "sources.default" },
+}
+

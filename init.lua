237b50a346973e7c7cee611fb14ed3280cd0a502
sleepylog <sleepylog@192.168.68.51>
2025-09-07 12:07:55 +0800

    stole sensible defaults from kickstart

--- init.lua
+++ init.lua
@@ -1,1 +1,47 @@
 require("config.lazy")
+
+local indentspace = 2
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.o.tabstop = indentspace
+vim.o.shiftwidth = indentspace
+vim.o.expandtab = true
+
+vim.keymap.set('n', '<leader>l', '<c-w>l')
+vim.keymap.set('n', '<leader>k', '<c-w>k')
+vim.keymap.set('n', '<leader>j', '<c-w>j')
+vim.keymap.set('n', '<leader>h', '<c-w>h')
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.signcolumn = 'yes'
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+vim.o.confirm = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})

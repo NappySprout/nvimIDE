ba09aafc4f5e1acf52c37553ca79fcadf3327096
chand <darie.chan@autodesk.com>
2025-09-09 19:07:19 +0800

    fixed setting up of lombok

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,68 @@
 return {
   'neovim/nvim-lspconfig',
   dependencies = {
-    {'mason-org/mason-lspconfig.nvim', opts = {automatic_enable = true}},
+    {
+      'mason-org/mason-lspconfig.nvim',
+      opts = {
+        automatic_enable = true,
+        ensure_installed = { "jdtls" },
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup({})
+          end,
+          -- Add a special handler for jdtls
+          jdtls = function()
+            local home = os.getenv('HOME')
+            local workspace_path = home .. '/.local/share/nvim/jdtls-workspace/'
+            local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+            local workspace_dir = workspace_path .. project_name
+
+            require("lspconfig").jdtls.setup({
+              -- Use root_markers for clean project root detection
+              root_markers = { 'pom.xml', 'build.gradle', '.git' },
+              cmd = {
+                '/Library/Java/JavaVirtualMachines/zulu-21.jdk/Contents/Home/bin/java',
+                '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+                '-Dosgi.bundles.defaultStartLevel=4',
+                '-Declipse.product=org.eclipse.jdt.ls.core.product',
+                '-Dlog.protocol=true',
+                '-Dlog.level=ALL',
+                '-Xmx1g',
+                '--add-modules=ALL-SYSTEM',
+                '--add-opens',
+                'java.base/java.util=ALL-UNNAMED',
+                '--add-opens',
+                'java.base/java.lang=ALL-UNNAMED',
+                '-javaagent:' .. home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar',
+                '-jar',
+                vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+                '-configuration',
+                home .. '/.local/share/nvim/mason/packages/jdtls/config_mac',
+                '-data',
+                workspace_dir,
+              },
+              settings = {
+                java = {
+                  referencesCodeLens = {
+                    enabled = true,
+                  },
+                  inlayHints = {
+                    parameterNames = {
+                      enabled = 'all',
+                    },
+                  },
+                },
+              },
+            })
+          end,
+        },
+      }
+    },
     {
       "folke/lazydev.nvim",
-      ft = "lua", -- only load on lua files
+      ft = "lua",
       opts = {
         library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
           { path = "${3rd}/luv/library", words = { "vim%.uv" } },
         },
       },
@@ -25,8 +79,8 @@ return {
     vim.api.nvim_create_autocmd('LspAttach', {
       callback = function(event)
         local map = function(keys, func, desc, mode)
-            mode = mode or 'n'
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          mode = mode or 'n'
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
         map('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
         map('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
@@ -34,4 +88,3 @@ return {
     })
   end
 }
--- :h lspconfig-all is helpful to enable lsp

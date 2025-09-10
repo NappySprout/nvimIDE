c1bbea0bb13e1af9a056f6c97278037165add64e
chand <darie.chan@autodesk.com>
2025-09-10 22:59:24 +0800

    added lsp fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,63 +2,6 @@ return {
   'neovim/nvim-lspconfig',
   dependencies = {
     {
-      'mason-org/mason-lspconfig.nvim',
-      opts = {
-        automatic_enable = true,
-        ensure_installed = { "jdtls" },
-        handlers = {
-          function(server_name)
-            require("lspconfig")[server_name].setup({})
-          end,
-          -- Add a special handler for jdtls
-          jdtls = function()
-            local home = os.getenv('HOME')
-            local workspace_path = home .. '/.local/share/nvim/jdtls-workspace/'
-            local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-            local workspace_dir = workspace_path .. project_name
-
-            require("lspconfig").jdtls.setup({
-              -- Use root_markers for clean project root detection
-              root_markers = { 'pom.xml', 'build.gradle', '.git' },
-              cmd = {
-                '/Library/Java/JavaVirtualMachines/zulu-21.jdk/Contents/Home/bin/java',
-                '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-                '-Dosgi.bundles.defaultStartLevel=4',
-                '-Declipse.product=org.eclipse.jdt.ls.core.product',
-                '-Dlog.protocol=true',
-                '-Dlog.level=ALL',
-                '-Xmx1g',
-                '--add-modules=ALL-SYSTEM',
-                '--add-opens',
-                'java.base/java.util=ALL-UNNAMED',
-                '--add-opens',
-                'java.base/java.lang=ALL-UNNAMED',
-                '-javaagent:' .. home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar',
-                '-jar',
-                vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
-                '-configuration',
-                home .. '/.local/share/nvim/mason/packages/jdtls/config_mac',
-                '-data',
-                workspace_dir,
-              },
-              settings = {
-                java = {
-                  referencesCodeLens = {
-                    enabled = true,
-                  },
-                  inlayHints = {
-                    parameterNames = {
-                      enabled = 'all',
-                    },
-                  },
-                },
-              },
-            })
-          end,
-        },
-      }
-    },
-    {
       "folke/lazydev.nvim",
       ft = "lua",
       opts = {
@@ -85,6 +28,48 @@ return {
         map('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
         map('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
       end
+    })
+    local home = os.getenv('HOME')
+    local workspace_path = home .. '/.local/share/nvim/jdtls-workspace/'
+    local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+    local workspace_dir = workspace_path .. project_name
+
+    vim.lsp.config('jdtls', {
+      -- Use root_markers for clean project root detection
+      root_markers = { 'pom.xml', 'build.gradle', '.git' },
+      cmd = {
+        '/Library/Java/JavaVirtualMachines/zulu-21.jdk/Contents/Home/bin/java',
+        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+        '-Dosgi.bundles.defaultStartLevel=4',
+        '-Declipse.product=org.eclipse.jdt.ls.core.product',
+        '-Dlog.protocol=true',
+        '-Dlog.level=ALL',
+        '-Xmx1g',
+        '--add-modules=ALL-SYSTEM',
+        '--add-opens',
+        'java.base/java.util=ALL-UNNAMED',
+        '--add-opens',
+        'java.base/java.lang=ALL-UNNAMED',
+        '-javaagent:' .. home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar',
+        '-jar',
+        vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+        '-configuration',
+        home .. '/.local/share/nvim/mason/packages/jdtls/config_mac',
+        '-data',
+        workspace_dir,
+      },
+      settings = {
+        java = {
+          referencesCodeLens = {
+            enabled = true,
+          },
+          inlayHints = {
+            parameterNames = {
+              enabled = 'all',
+            },
+          },
+        },
+      },
     })
   end
 }
